e95aabc5bb37748b0e550d97c50cca04fcd767be
josaphat Mosha <ngimonyijos@gmail.com>
2023-02-23 21:30:56 +0100

    finished InputMessage component

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -5,11 +5,15 @@ const ChatRoom = () => {
 
     const messages = [1, 2, 3];
 
+    const handleInputMessage = (message) => {
+        // ovdje ovu poruku trebamo posati na Scaledrone
+    }
+
     return <div>
         {messages.map(msg => {
             return <Message />
         })}
-        <MessageInput />
+        <MessageInput onMessage={handleInputMessage} />
     </div>
 }
 
--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,6 +1,20 @@
-const MessageInput = () => {
+import { useState, useEffect } from "react";
+
+const MessageInput = ({ onMessage }) => {
+    const [message, setMessage] = useState("");
+
+    const handleClick = () => {
+        // ako consumer ove komponente nije u props poslao 'onMessage' onda zanemarimo slanje poruke
+        if (onMessage) {
+            onMessage(message);
+        }
+        // nakon što kliknemo na 'Send' button, očistimo <input />
+        setMessage("");
+    }
+
     return <div>
-        Ovo je MessageInput
+        <input type={"text"} value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={handleClick}>Send</button>
     </div>
 }
 
